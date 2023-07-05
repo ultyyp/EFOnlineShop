1e6a527273e7459543cb99326a1f6b88f7337184
ultyyp <yehia.super@gmail.com>
2023-07-05 16:40:04 +0300

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace EFOnlineShop.Data
+{
+    using EFOnlineShop.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : DbContext
+	{
+		//Список таблиц:
+		public DbSet<Product> Products => Set<Product>();
+
+		public AppDbContext(
+			DbContextOptions<AppDbContext> options)
+			: base(options)
+		{
+		}
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+using EFOnlineShop.Data;
+using EFOnlineShop.Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Database
+var dbPath = "myapp.db";
+builder.Services.AddDbContext<AppDbContext>(
+   options => options.UseSqlite($"Data Source={dbPath}"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+//C
+app.MapPost("/add_product", AddProduct);
+
+//R
+app.MapGet("/get_products", async (AppDbContext context)
+   => await context.Products.ToListAsync());
+
+//U
+app.MapPost("/update_product", UpdateProduct);
+
+//D
+app.MapDelete("/delete_product", DeleteProduct);
+
+
+
+async Task AddProduct(AppDbContext db, Product product)
+{
+	await db.Products.AddAsync(product);
+	await db.SaveChangesAsync();
+}
+
+async Task UpdateProduct(AppDbContext db, Guid productId, Product product)
+{
+	var prod = await db.Products.SingleOrDefaultAsync(p => p.Id == productId);
+	db.Entry(prod).CurrentValues.SetValues(product);
+	await db.SaveChangesAsync();
+}
+
+async Task DeleteProduct(AppDbContext db, Guid productId)
+{
+	db.Products.Remove(await db.Products.SingleOrDefaultAsync(p => p.Id == productId));
+	await db.SaveChangesAsync();
+}
+
+
+app.Run();
